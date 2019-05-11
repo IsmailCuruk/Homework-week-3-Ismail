a0ceba138457e8e862aa5c9f0c98f4e750261f1b
IsmailCuruk <ismailcrk@gmail.com>
2019-05-11 20:20:08 +0200

    Assignment 1 (only worked after tweaking the data)

--- src/App.js
+++ src/App.js
@@ -1,44 +1,62 @@
 import React from 'react';
 import './App.css';
 
+const data = [{
+  id: 1,
+  name: "Ivel Z3",
+  manufacturer: "Ivasim",
+  year: 1969,
+  origin: "Croatia"
+},
+{
+  id: 2,
+  name: "Bally Astrocade",
+  manufacturer: "Bally Consumer Products",
+  year: 1977,
+  origin: "USA"
+},
+{
+  id: 3,
+  name: "Sord M200 Smart Home Computer",
+  manufacturer: "Sord Computer Corporation",
+  year: 1971,
+  origin: "Japan"
+},
+{
+  id: 4,
+  name: "Commodore 64",
+  manufacturer: "Commodore",
+  year: 1982,
+  origin: "USA"
+}]
+
+
+
+class Computer extends React.Component {
+  renderComputer = (computer) => {
+    return <option key={computer.id} value={computer.name}>{computer.name} ({computer.year})</option>
+  }
+  render() {
+    return (
+      <select>
+        <option value="">-- pick a model --</option>
+        {data.map(this.renderComputer)}
+      </select>
+    )
+
+  }
+};
+
 
 class App extends React.Component {
   render() {
-    const data = {
-      "Ivel Z3": {
-        manufacturer: "Ivasim",
-        year: 1969,
-        origin: "Croatia"
-      },
-      "Bally Astrocade": {
-        manufacturer: "Bally Consumer Products",
-        year: 1977,
-        origin: "USA"
-      },
-      "Sord M200 Smart Home Computer": {
-        manufacturer: "Sord Computer Corporation",
-        year: 1971,
-        origin: "Japan"
-      },
-      "Commodore 64": {
-        manufacturer: "Commodore",
-        year: 1982,
-        origin: "USA"
-      }
-    }
-
     return (
-      <div className="App">
-        <select>
-        <option value="">-- pick a model --</option>
-          <option value=""></option>
-          <option value=""></option>
-          <option value=""></option>
-          <option value=""></option>
-        </select>
+      <div className="App" >
+        <Computer />
       </div>
     );
   }
 }
+
 
 export default App;
